b94480099fb346684529bf816d502b022354053c
Nerdherdx <nerdherdx212@gmail.com>
2016-06-10 17:55:57 -0400

    Password generator file

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-/**
- * Created by Jim on 6/10/2016.
- */
+$(document).ready(function () {
+    $("#pass").on("click",function(){
+       password();
+    });
+
+
+});
+
+    function password() {
+
+
+        var char = "JmPo3SDg8GhdJHFHSKdfh548365jFI9gjFG";
+        var password = "";
+        var len = $("#password").val();
+
+
+        for (var i = 0; i < len;i++) {
+
+            password += char.charAt(Math.floor(Math.random() * char.length));
+
+            $("#newPass").text(password);
+
+
+
+        }
+
+    }
+password();
